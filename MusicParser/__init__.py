03be876e9a858917680cab5f51209a14d5c488a9
Yungon Park <hahafree12@gmail.com>
2017-12-17 00:06:24 +0900

    Parser 패키지 구성 및 setup 스크립트 작성

---
+++ MusicParser/__init__.py
@@ -0,0 +1,9 @@
+"""
+Music Parser
+2016~2017 Yungon Park (hahafree12@gmail.com)
+--------------------------------------------
+Parser to get a information from music sites.
+Supporting
+"""
+
+__all__ = ['parser']
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MusicParser',
+    version='0.0.1',
+    url='https://github.com/rubysoho07/MusicParser',
+    author='Yungon Park',
+    author_email='hahafree12@gmail.com',
+    description='Parsing music album from music information sites.',
+    packages=find_packages()
+)

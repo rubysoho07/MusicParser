8cceb591de560d03ebfc7044ed061b357c0e8433
Yungon Park <hahafree12@gmail.com>
2017-12-17 09:37:29 +0900

    설명 추가

--- MusicParser/__init__.py
+++ MusicParser/__init__.py
@@ -3,7 +3,7 @@ Music Parser
 2016~2017 Yungon Park (hahafree12@gmail.com)
 --------------------------------------------
 Parser to get a information from music sites.
-Supporting
+Supporting Melon, Bugs, Naver Music and AllMusic.
 """
 
 __all__ = ['parser']

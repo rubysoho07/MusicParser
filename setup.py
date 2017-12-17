b7191b8efc5827dc6becd25f3863cf820effdf55
Yungon Park <hahafree12@gmail.com>
2017-12-17 09:31:43 +0900

    패키지 필요조건 추가 및 0.0.2 버전으로 업그레이드

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='MusicParser',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/rubysoho07/MusicParser',
     author='Yungon Park',
     author_email='hahafree12@gmail.com',
     description='Parsing music album from music information sites.',
+    install_requires=[
+        "requests == 2.18.4",
+        "beautifulsoup4 == 4.6.0"
+    ],
     packages=find_packages()
 )

321ea4c39d5abca0b9139b8dfdd13d421fbd280b
Yungon Park <hahafree12@gmail.com>
2018-11-03 00:25:17 +0900

    requests 라이브러리 버전을 requirements.txt 파일과 맞춤

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='hahafree12@gmail.com',
     description='Parsing music album from music information sites.',
     install_requires=[
-        "requests == 2.18.4",
+        "requests == 2.20.0",
         "beautifulsoup4 == 4.6.0"
     ],
     packages=find_packages()
